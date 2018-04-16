9d2ea7ba6a269ae8dacf610c01d543f9777878e7
nagatsukey <nagatsukey@gmail.com>
2018-04-16 14:28:34 +0900

    routerにあったデータ処理をcontrollerに移譲

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,17 +1,39 @@
 package controllers
 import (
     "gin-api-server-sandbox/models"
+    "github.com/gin-gonic/gin"
+    "reflect"
+    "strconv"
 )
 // User is
 type User struct {
 }
-// NewUser ...
-func NewUser() User {
-    return User{}
-}
+
 // Get ...
 func (c User) Get(n int) interface{} {
     repo := models.NewUserRepository()
     user := repo.GetByID(n)
     return user
+}
+
+func GetUser(c *gin.Context) {
+    // Pramを処理する
+    n := c.Param("id")
+    id, err := strconv.Atoi(n)
+    if err != nil {
+        c.JSON(400, err)
+        return
+    }
+    if id <= 0 {
+        c.JSON(400, gin.H{"status": "id should be bigger than 0"})
+        return
+    }
+    // データを処理する
+    user := User{}
+    result := user.Get(id)
+    if result == nil || reflect.ValueOf(result).IsNil() {
+        c.JSON(404, gin.H{})
+        return
+    }
+    c.JSON(200, result)
 }
--- main.go
+++ main.go
@@ -2,35 +2,10 @@ package main
 import (
     "gin-api-server-sandbox/controllers"
     "github.com/gin-gonic/gin"
-    "reflect"
-    "strconv"
-    "fmt"
 )
 // main ...
 func main() {
-    fmt.Printf("testet/n")
     router := gin.Default()
-    router.GET("/:id", func(c *gin.Context) {
-        // Pramを処理する
-        n := c.Param("id")
-        id, err := strconv.Atoi(n)
-        if err != nil {
-            c.JSON(400, err)
-            return
-        }
-        if id <= 0 {
-            c.JSON(400, gin.H{"status": "id should be bigger than 0"})
-            return
-        }
-        // データを処理する
-        ctrl := controllers.NewUser()
-        fmt.Printf("%+v", ctrl)
-        result := ctrl.Get(id)
-        if result == nil || reflect.ValueOf(result).IsNil() {
-            c.JSON(404, gin.H{})
-            return
-        }
-        c.JSON(200, result)
-    })
+    router.GET("/:id", func(c *gin.Context) { controllers.GetUser(c)})
     router.Run(":8000")
 }
